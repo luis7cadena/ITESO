ad6b5cff55988723fc9bbfa4225d6dc4c3f02df3
Vladimir <rt683522@iteso.mx>
2013-01-24 08:36:18 -0600

    comentario

--- hola.c
+++ hola.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
-void	main(void)	{
-printf(“Hola	Mundo\n”);
-}
 
-void funcion1(void){
-printf("Funcion 1\n");
+#include
+<stdio.h>
+void
+main(void)
+{
+printf("Hola
+Mundo\n");
+}
+void
+funcion2(void)
+{
+printf("Funcion
+2\n")
+}
+void
+funcion1(void)
+{
+printf("Funcion
+1\n");
 }
